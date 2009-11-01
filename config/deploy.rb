513d68bf387115d7f9aced728999b4f194e7329b
kashif <kashif@nomad-labs.com>
2009-11-01 15:48:51 +0100

    added initial capistrano deploy script

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+set :application, "holmesplace"
+set :repository,  "git@github.com:kashif/holmesplace.git"
+set :deploy_to,   "/data/kashif/#{application}"
+
+set :scm,   :git
+set :user,  "kashif"
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "nomad-labs.dyndns.org"                          # Your HTTP server, Apache/etc
+role :app, "nomad-labs.dyndns.org"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start {}
+#   task :stop {}
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
